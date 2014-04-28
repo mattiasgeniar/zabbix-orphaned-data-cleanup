704524a45364ece4d09519813eb9488f22dfb680
Ilya Ivanov <forn@ngs.ru>
2014-04-28 19:39:41 +0000

    add script to check unused data

---
+++ check-unused-data.sql
@@ -0,0 +1,8 @@
+SELECT count(itemid) AS history FROM history WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+SELECT count(itemid) AS history_uint FROM history_uint WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+SELECT count(itemid) AS history_str FROM history_str WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+SELECT count(itemid) AS history_text FROM history_text WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+SELECT count(itemid) AS history_log FROM history_log WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+
+SELECT count(itemid) AS trends FROM trends WHERE itemid NOT IN (SELECT itemid FROM items WHERE status='0');
+SELECT count(itemid) AS trends FROM trends itemid NOT IN (SELECT itemid FROM items WHERE status='0');
