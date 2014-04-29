59d75624b72e47e42a25954e53ca2327a33be55d
Ilya Ivanov <forn@ngs.ru>
2014-04-29 18:55:17 +0000

    fix one more typo in delete

--- delete-old-data.my.sql
+++ delete-old-data.my.sql
@@ -3,7 +3,7 @@ SET @history_interval = 7;
 SET @trends_interval = 90;
 
 DELETE FROM alerts WHERE (UNIX_TIMESTAMP(NOW()) - clock) > (@history_interval * 24 * 60 * 60);
-SELECT count(acknowledgeid) AS acknowledges FROM acknowledges WHERE (UNIX_TIMESTAMP(NOW()) - clock) > (@history_interval * 24 * 60 * 60);
+DELETE FROM acknowledges WHERE (UNIX_TIMESTAMP(NOW()) - clock) > (@history_interval * 24 * 60 * 60);
 DELETE FROM events WHERE (UNIX_TIMESTAMP(NOW()) - clock) > (@history_interval * 24 * 60 * 60);
 
 DELETE FROM history WHERE (UNIX_TIMESTAMP(NOW()) - clock) > (@history_interval * 24 * 60 * 60);
